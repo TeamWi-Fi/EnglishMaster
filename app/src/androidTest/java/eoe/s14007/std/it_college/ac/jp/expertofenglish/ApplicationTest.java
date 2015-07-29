c23332de66fba4a396fe4a94ec9b0cda6a7e665a
s14007 <s14007@std.it-college.ac.jp>
2015-07-29 16:16:49 +0900

    first commit

---
+++ app/src/androidTest/java/eoe/s14007/std/it_college/ac/jp/expertofenglish/ApplicationTest.java
@@ -0,0 +1,13 @@
+package eoe.s14007.std.it_college.ac.jp.expertofenglish;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
