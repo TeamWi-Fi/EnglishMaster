dc44ff535b41a3eb0033e329c5d8f2e9d077f45a
s14002 <s14002@std.it-college.ac.jp>
2015-08-19 15:23:14 +0900

    create 4 button , labelCurrentQuestion and fix activity_score.xml

--- app/src/main/java/eoe/s14007/std/it_college/ac/jp/expertofenglish/ScoreActivity.java
+++ app/src/main/java/eoe/s14007/std/it_college/ac/jp/expertofenglish/ScoreActivity.java
@@ -2,11 +2,12 @@ package eoe.s14007.std.it_college.ac.jp.expertofenglish;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ScoreActivity extends ActionBarActivity {
+public class ScoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
